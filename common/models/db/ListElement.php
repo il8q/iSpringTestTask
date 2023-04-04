23c2ae1b2e0a1d44810f3e3eb869f44738ba7498
DESKTOP-KC00KL8\Илья <ilarm8q@gmail.com>
2023-04-04 14:17:31 +0300

    Исправлены правила в ListElement

--- common/models/db/ListElement.php
+++ common/models/db/ListElement.php
@@ -2,8 +2,6 @@
 
 namespace common\models\db;
 
-use Yii;
-
 /**
  * This is the model class for table "listElement".
  *
@@ -15,13 +13,52 @@ use Yii;
  */
 class ListElement extends \common\models\db\BaseListElement
 {
+    public function beforeSave($insert): bool
+    {
+        if (!parent::beforeSave($insert)) {
+            return false;
+        }
+        if (empty($this->createdAt)) {
+            $this->createdAt = time();
+        }
+        if (empty($this->updatedAt)) {
+            $this->updatedAt = time();
+        }
+        return true;
+    }
+
     /**
      * {@inheritdoc}
      */
     public function rules(): array
     {
-        return array_merge([
-            [['description'], 'length' => 1000],
+        return array_merge(parent::rules(), [
+            [['description'], 'required', 'message' => 'Сообщение не может быть пустым'],
+            ['description',
+                'string',
+                'min' => 2,
+                'max' => 1000,
+                'tooShort' => 'Сообщение может быть пустым',
+                'tooLong' => 'Сообщение должно быть не длинее 1000 символов',
+            ],
+            ['isCompleted', 'default', 'value' => 0],
         ]);
+    }
+
+    public function serializeShortToArray(): array
+    {
+        $result = [];
+        $result['listElementId'] = (int)$this->listElementId;
+        $result['description'] = $this->description;
+        $result['isCompleted'] = (bool)$this->isCompleted;
+        return $result;
+    }
+
+    public function serializeToArray(): array
+    {
+        $result = $this->serializeShortToArray();
+        $result['createdAt'] = (int)$this->createdAt;
+        $result['updatedAt'] = (int)$this->updatedAt;
+        return $result;
     }
 }
